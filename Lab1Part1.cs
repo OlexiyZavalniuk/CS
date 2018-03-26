595b4fd08e97696ea9c275c9d90fd524b1d45bae
Olexiy Zavalniuk <32261728+OlexiyZavalniuk@users.noreply.github.com>
2018-03-26 23:12:39 +0300

    Add files via upload

---
+++ Lab1Part1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSLab1Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введіть посилання на документ:");
+            string dir = Console.ReadLine();
+            List<string> symbols = new List<string>();
+            List<int> numbers = new List<int>();
+            CalculateText(dir, symbols, numbers);
+            ShowResult(dir, symbols, numbers);
+            Console.Read();
+        }
+        static void CalculateText(string dir, List<string> symbols, List<int> numbers)
+        {
+            string symbol = "", line = "";
+            using (TextReader tr = new StreamReader(dir))
+            {
+                while (tr.Peek() != -1)
+                {
+                    line = tr.ReadLine();
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        symbol = line.Substring(i, 1);
+                        //symbol = symbol.ToLower();
+                        if (numbers.Count == 0)
+                        {
+                            symbols.Add(symbol);
+                            numbers.Add(1);
+                        }
+                        else
+                        {
+                            for (int q = 0; q < symbols.Count; q++)
+                            {
+                                if (symbols[q] == symbol)
+                                {
+                                    numbers[q]++;
+                                    break;
+                                }
+                                else if (q == symbols.Count - 1)
+                                {
+                                    symbols.Add(symbol);
+                                    numbers.Add(1);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        static void ShowResult(string dir, List<string> symbols, List<int> numbers)
+        {
+            Console.WriteLine("Файл: " + dir);
+            Console.WriteLine("Кількість всіх символів у файлі: {0}", Number(numbers));
+            for (int i = 0; i < symbols.Count; i++)
+            {
+                Console.WriteLine("Символ '{0}' -> кількість {1} -> ймовірність появи {2}%", symbols[i], numbers[i], Chance(numbers, i));
+            }
+            double temp_entropy = Entropy(numbers);
+            Console.WriteLine("Ентропія: {0}", temp_entropy);
+            Console.WriteLine("Кількість інформації: {0} | {1}", AmountOfInformation(temp_entropy, Number(numbers)), new FileInfo(dir).Length);
+        }
+        static double Chance(List<int> numbers, int num)
+        {
+            double chance = ((double)numbers[num] / (double)Number(numbers)) * 100;
+            return chance;
+        }
+        static double Entropy(List<int> numbers)
+        {
+            double entropy = 0;
+            double p = 0;
+            //result = sum (1 to m) p*log2(1/p)
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                p = (double)numbers[i] / (double)Number(numbers);
+                if (p != 0)
+                {
+                    entropy += p * Math.Log((double)1 / p, 2);
+                }
+            }
+            return entropy;
+        }
+        static int AmountOfInformation(double entropy, int amount)
+        {
+            int am_bits = (int)Math.Ceiling(entropy * amount);
+            return (int)Math.Ceiling((double)am_bits / (double)8);//returns result in bytes
+        }
+        static int Number(List<int> numbers)
+        {
+            int num = 0;
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                num += numbers[i];
+            }
+            return num;
+        }
+    }
+}
---
+++ Lab1Part2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введіть посилання на файл який закодувати:");
+            string inDir = Console.ReadLine();
+            Console.WriteLine("Введіть посилання на файл який створити:");
+            string outDir = Console.ReadLine();
+            FillAlphabet();
+            CodeFileToBase64(inDir, outDir);
+        }
+        static Dictionary<int, char> base64Alphabet = new Dictionary<int, char>();
+        static void FillAlphabet()
+        {
+            int value = 65, key = 0;
+            for (; key <= 25; key++)
+            {
+                base64Alphabet.Add(key, (char)value);
+                value++;
+            }
+            value = 97;
+            for (key = 26; key <= 51; key++)
+            {
+                base64Alphabet.Add(key, (char)value);
+                value++;
+            }
+            value = 48;
+            for (key = 52; key <= 61; key++)
+            {
+                base64Alphabet.Add(key, (char)value);
+                value++;
+            }
+            base64Alphabet.Add(62, '+');
+            base64Alphabet.Add(63, '/');
+        }
+
+        static bool[] IntToBinary(int value)
+        {
+            bool[] b = new bool[8];
+            for (int i = 7; i >= 0; i--)
+            {
+                b[i] = (value % 2 == 1) ? true : false;
+                value /= 2;
+            }
+            return b;
+        }
+
+        static bool[] ReadBitsFromFile(string path)
+        {
+            byte[] data = File.ReadAllBytes(path);
+            bool[] bits = new bool[data.Length * 8], temp;
+            for (int i = data.Length - 1; i >= 0; i--)
+            {
+                temp = IntToBinary(data[i]);
+                for (int l = 0; l < temp.Length; l++)
+                    bits[i * 8 + l] = temp[l];
+            }
+            return bits;
+        }
+
+        static int BitInIndex(bool[] bits)
+        {
+            int decNumber = 0;
+            for (int i = 0; i < bits.Length; i++)
+                if (bits[i])
+                    decNumber += (int)Math.Pow(2, bits.Length - 1 - i);
+            return decNumber;
+        }
+        static void CodeFileToBase64(string path, string newpath)
+        {
+            using (StreamWriter wr = new StreamWriter(newpath, false))
+                wr.Write(ToBase64(ReadBitsFromFile(path)));
+        }
+
+        static string ToBase64(bool[] b)
+        {
+            string data = "", pad = "";
+            bool[] temp = new bool[6];
+            for (int i = 0; i < b.Length; i += 6)
+            {
+                for (int k = 0; k < temp.Length; k++)
+                {
+                    if (i + k < b.Length)
+                        temp[k] = b[i + k];
+                    else
+                    {
+                        if (b.Length % 3 == 2)
+                        {
+                            temp[k + 1] = temp[k + 2] = temp[k + 3] = temp[k] = false;
+                            pad = "==";
+                            k = 6;
+                        }
+                        else
+                        {
+                            temp[k + 1] = temp[k] = false;
+                            pad = "=";
+                            k = 6;
+                        }
+                    }
+                }
+                data += base64Alphabet[BitInIndex(temp)].ToString();
+            }
+            return data + pad;
+        }
+    }
+}
+
+
+
